661800bfa663b641c778854fd30630c71fc22242
Kean <kenethanders@gmail.com>
2022-11-14 14:15:45 +0100

    Init

---
+++ PWA_Blazor/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace PWA_Blazor.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<string> Pictures { get; set; }
+
+
+        public User()
+        {
+            Pictures = new List<string>();
+        }
+    }
+}
